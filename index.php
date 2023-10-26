78ab5b0db304a6c0f0af6e5091fc1422c4d8a3c0
Ayela Emmanuel <62917075+lilwizzi9@users.noreply.github.com>
2023-10-26 14:00:40 +0100

    Index Page

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//Super Globals
+var_dump($_POST);
+?>
+
+
+<form method="post">
+    <input type="text" name="username"><br>
+    <input type="text" name="useremail"><br>
+    <button>Submit</button>
+</form>
